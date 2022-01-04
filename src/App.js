e1e70cc94ab3cfe5a2023955b569fcd44598b6be
gitgood-ENT <ward.noahd@gmail.com>
2022-01-04 15:50:53 -0800

    created 3x3 gameboard with ?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from "./components/Square";
 
 class App extends Component{
   constructor(props){
@@ -13,6 +14,9 @@ class App extends Component{
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div className="gameBoard">
+          {this.state.board.map(value => value = <Square value={value}/>)}
+        </div>
       </>
     )
   }
